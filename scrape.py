5c094b83f816e31667b406f7cfc7dff3345ac7f4
raaasin <nisarvskp@gmail.com>
2023-07-18 20:44:20 +0530

    prettified codechef

--- scrape.py
+++ scrape.py
@@ -12,22 +12,7 @@ if response.status_code == 200:
     # Create a BeautifulSoup object from the response content
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Find the streak element by searching for the 'Streak' label
-    streak_label = soup.find('b', text='Streak:')
-    if streak_label:
-        # Extract the streak value from the following sibling element
-        streak = streak_label.next_sibling.strip()
-        print("Streak:", streak)
-    else:
-        print("Streak information not found on the page.")
-
-    # Find the problems solved element by searching for the 'Problems Solved' label
-    problems_label = soup.find('b', text='Problems Solved:')
-    if problems_label:
-        # Extract the problems solved value from the following sibling element
-        problems_solved = problems_label.next_sibling.strip()
-        print("Problems Solved:", problems_solved)
-    else:
-        print("Problems solved information not found on the page.")
+    # Print the prettified HTML content
+    print(soup.prettify())
 else:
     print("Failed to retrieve data from CodeChef.")
