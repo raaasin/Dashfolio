7951a58f90925a57d4ce82adba1c54594c2a76b7
raaasin <nisarvskp@gmail.com>
2023-07-18 20:52:34 +0530

    extraction

--- scrape.py
+++ scrape.py
@@ -12,7 +12,24 @@ if response.status_code == 200:
     # Create a BeautifulSoup object from the response content
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Print the prettified HTML content
-    print(soup.prettify())
+    # Find the streak section
+    streak_section = soup.find('div', class_='user-profile-container')
+    if streak_section:
+        # Save the streak section to a text file
+        with open('streak_section.html', 'w', encoding='utf-8') as file:
+            file.write(streak_section.prettify())
+        print("Streak section saved to 'streak_section.html' file.")
+    else:
+        print("Streak information section not found on the page.")
+
+    # Find the problems section
+    problems_section = soup.find('div', class_='user-profile-container')
+    if problems_section:
+        # Save the problems section to a text file
+        with open('problems_section.html', 'w', encoding='utf-8') as file:
+            file.write(problems_section.prettify())
+        print("Problems section saved to 'problems_section.html' file.")
+    else:
+        print("Problems solved section not found on the page.")
 else:
     print("Failed to retrieve data from CodeChef.")
